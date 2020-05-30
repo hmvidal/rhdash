f546fd46595986b414a910d7ad6d08acad4fd55a
Hernando M. Vidal <nando@hmvidal.io>
2020-05-30 16:28:20 -0400

    Add EMA support

--- rhdash/rhdash.py
+++ rhdash/rhdash.py
@@ -10,6 +10,7 @@ import robin_stocks
 from dash.dependencies import Input
 from dash.dependencies import Output
 
+from rhdash.alg import get_n_ema
 from rhdash.args import setup_args
 from rhdash.config import fetch
 
@@ -18,17 +19,17 @@ def setup_dash(config):
     """Set up dashboard server."""
 
     dash_config = config["dash"]
-    external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+    external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
     app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
     dash_auth.BasicAuth(
         app, {dash_config["creds"]["user"]: dash_config["creds"]["password"]})
 
     app.layout = html.Div([
         html.Div(children="Symbol:"),
-        dcc.Input(id='symbol', value='', type='text'),
-        # html.H1("Dashboard"),
+        dcc.Input(id="symbol", value="", type="text"),
+        html.H1(id="heading", children=""),
         dcc.Graph(id="value-graph"),
-        # dcc.Graph(id="diff-graph")
+        dcc.Graph(id="diff-graph")
     ])
 
     return app
@@ -57,16 +58,37 @@ def run_with(arguments):
         configuration = fetch(arguments.config)
         app = init_using(configuration)
 
-        @app.callback(Output('value-graph', 'figure'),
-                      [Input('symbol', 'value')])
+        @app.callback([
+            Output("heading", "children"),
+            Output("value-graph", "figure"),
+            Output("diff-graph", "figure")
+        ], [Input("symbol", "value")])
         def update_figure(symbol):
             try:
                 symbol = str(symbol).strip().upper()
                 name = robin_stocks.stocks.get_name_by_symbol(
                     symbol) if symbol != "" else ""
+                heading = f"{name} ({symbol})" if len(name) > 0 else ""
+
                 year_data = robin_stocks.stocks.get_historicals(symbol,
                                                                 span="year")
                 df = pd.DataFrame(year_data)
+                float_cols = [
+                    "open_price", "close_price", "high_price", "low_price"
+                ]
+                for col in float_cols:
+                    df[col] = df[col].astype(float)
+
+                for n_days in configuration["robinhood"]["ema_days"]:
+                    df[f"ema_{n_days}"] = 0.0
+                    for i, row in df.iterrows():
+                        if i <= (n_days - 1):
+                            ema = df.iloc[0:i + 1]["close_price"].sum() / (i +
+                                                                           1)
+                        else:
+                            ema = get_n_ema(n_days, row["close_price"],
+                                            df.iloc[i - 1]["close_price"])
+                        df.at[i, f"ema_{n_days}"] = ema
 
                 n_total_days = len(df["begins_at"])
                 window_boundary = n_total_days * 4 / 9
@@ -78,22 +100,57 @@ def run_with(arguments):
                     "type": "line",
                     "name": "close_price"
                 }]
-            except Exception:
+
+                diff_graph_data = []
+
+                for n_days in configuration["robinhood"]["ema_days"]:
+                    data = {
+                        "x": df["begins_at"],
+                        "y": df[f"ema_{n_days}"],
+                        "type": "line",
+                        "name": f"ema_{n_days}"
+                    }
+                    value_graph_data.append(data)
+                    diff_data = {
+                        "x":
+                        df["begins_at"],
+                        "y":
+                        100.0 * (df["close_price"] - df[f"ema_{n_days}"]) /
+                        df["close_price"],
+                        "type":
+                        "line",
+                        "name":
+                        f"ema_{n_days}_diff"
+                    }
+                    diff_graph_data.append(diff_data)
+            except Exception as e:
+                print(f"Could not update data for '{symbol}'.")
                 value_graph_data = [{
                     "x": [],
                     "y": [],
                     "type": "line",
-                    "name": "close_price"
+                    "name": ""
+                }]
+                diff_graph_data = [{
+                    "x": [],
+                    "y": [],
+                    "type": "line",
+                    "name": ""
                 }]
 
-            return {
-                'data': value_graph_data,
-                'layout': {
-                    "title": f"{name} ({symbol})"
+            return heading, {
+                "data": value_graph_data,
+                "layout": {
+                    "title": f"{name} Close Price"
+                }
+            }, {
+                "data": diff_graph_data,
+                "layout": {
+                    "title": f"Close Price minus EMA (% diff)"
                 }
             }
 
-        app.run_server()
+        app.run_server(debug=True)
         return True
 
     return False
