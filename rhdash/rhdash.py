30172e195d085d1d559caa15065d9e85713cd606
Hernando M. Vidal <nando@hmvidal.io>
2020-05-29 23:20:48 -0400

    Add template to check config against

--- rhdash/rhdash.py
+++ rhdash/rhdash.py
@@ -1,6 +1,4 @@
 """Module dosctring"""
-import sys
-
 import dash
 import dash_auth
 import dash_html_components as html
@@ -11,28 +9,30 @@ from rhdash.config import fetch
 
 def setup_dash(config):
     """Set up dashboard server."""
+
     external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
     app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-    if "user" not in config or "password" not in config:
-        print("Please provide 'user' and 'password' in your configuration.")
-        sys.exit(1)
-    credentials = {config["user"]: config["password"]}
-    auth = dash_auth.BasicAuth(app, credentials)
-    assert auth
+
+    dash_config = config["dash"]
+    # robinhood_config = config["robinhood"]
+
+    dash_auth.BasicAuth(
+        app, {dash_config["creds"]["user"]: dash_config["creds"]["password"]})
     app.layout = html.Div(children=[html.H1(children="Dashboard")])
+
     return app
 
 
-def run():
+def run_with(arguments):
     """Main entrypoint."""
-    args = setup_args()
-    if args:
-        config = fetch(args.config)
-        app = setup_dash(config)
+    if arguments:
+        configuration = fetch(arguments.config)
+        app = setup_dash(configuration)
         app.run_server()
         return True
     return False
 
 
 if __name__ == "__main__":
-    run()
+    arguments = setup_args()
+    run_with(arguments)
